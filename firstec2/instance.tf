38b76f3908847bdcdad6e122db1847479a85d41a
kt.lamkhade <kt.lamkhade@gmail.com>
2022-08-02 19:23:45 +0530

    security group

--- firstec2/instance.tf
+++ firstec2/instance.tf
@@ -1,8 +1,24 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0729e439b6769d6ab"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0729e439b6769d6ab"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "HelloWorld"
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443]
+    iterator = prot
+    content {
+          description      = "TLS from VPC"
+          from_port        = prot.value
+          to_port          = port.value
+          protocol         = "tcp"
+          cidr_blocks      = [0.0.0.0/0]
+    }
   }
 }
